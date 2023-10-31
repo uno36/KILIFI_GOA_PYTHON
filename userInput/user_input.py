881d4dbf373fc213248831580aecd7566664a453
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-10-31 09:42:56 +0200

    Answer assignment question

---
+++ userInput/user_input.py
@@ -0,0 +1,11 @@
+# Ask the user for their name and store it in the "name" variable
+name = input("Please enter your name: ")
+
+# Ask the user for their age and store it in the "age" variable
+age = input("Please enter your age: ")
+
+# Ask the user for their location and store it in the "location" variable
+location = input("Please enter your location: ")
+
+# Print out a personalized message
+print(f"Hello {name}, you are {age} years old and you live in {location}.")
